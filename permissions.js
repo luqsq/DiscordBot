fadd0370639b3eefeca20f1e22e6164367b088c0
Keisimm <71936400+Keisimm@users.noreply.github.com>
2022-02-15 22:53:49 +0100

    Poprawa permisji

--- permissions.js
+++ permissions.js
@@ -11,12 +11,12 @@ module.exports = {
         '685841362918309907': { lvl: 3, type: 1 }, //jrtech
         '457627513355763744': { lvl: 2, type: 1 }, //helper
         '457808838628999170': { lvl: 6, type: 2 }, //admin forum
-        '886595380920348762': { lvl: 5, type: 2 }, //technik forum
         '457628493677592576': { lvl: 4, type: 2 }, //smod forum
         '457628500095008779': { lvl: 3, type: 2 }, //mod forum
+        '886595380920348762': { lvl: 3, type: 2 }, //technik forum
         '457628503576281088': { lvl: 2, type: 2 }, //test mod forum
-        '457625643987763211': { lvl: 4, type: 3 }, //technik ts
         '457630013249028098': { lvl: 3, type: 3 }, //mod ts
+        '457625643987763211': { lvl: 3, type: 3 }, //technik ts
         '457630041048612865': { lvl: 2, type: 3 }, //pomocnik ts
     },
     mute: 2,
