3f335c69c9bf9fb642d375457acd1326ffc46ad3
Keisimm <71936400+Keisimm@users.noreply.github.com>
2022-06-08 12:22:26 +0200

    Aktualizacja komendy !wakacje

--- commands/public/wakacje.js
+++ commands/public/wakacje.js
@@ -2,7 +2,16 @@ module.exports = {
     name: 'wakacje',
     desc: 'Za ile wakacje (lub koniec)...?',
     run: msg => {
-        const days = Math.floor((new Date(2022, 05, 24, 12).getTime() - Date.now())/86400000);
-        msg.channel.send(`☀️ Wakacje za **${days}** dni! ☀️`);
+        const today = new Date(2023, 5, 24);
+        const unix = today.getTime();
+        const year = today.getFullYear() + (today.getMonth() > 7 ? 1 : 0);
+        const date = new Date(year, 6, 0, 12);
+        const d = date.getDay();
+        let day = date.getDate() - (d == 0 ? 7 : d) + 5;
+        if(day - 7 > 20) day -= 7;
+        date.setDate(day);
+        const date2 = new Date(year, 8, 1, 9);
+        if(unix < date.getTime()) msg.channel.send(`☀️ Wakacje za **${Math.floor((date - unix)/86400000)}** dni! ☀️`);
+        else msg.channel.send(`☀️ Koniec wakacji za **${Math.floor((date2 - unix)/86400000)}** dni! ☀️`);
     }
 }
