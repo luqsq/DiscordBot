9f39fcf50fcde8535e824a530ed149f0102e5617
Keisimm <71936400+Keisimm@users.noreply.github.com>
2022-06-21 18:55:23 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = {
     event: 'ready',
-    run: () => {
-        console.log('Bot aktywny, wersja v1.2.4');
+    run: (client) => {
+        console.log('Bot aktywny, wersja v' + client.version);
     }
 }
